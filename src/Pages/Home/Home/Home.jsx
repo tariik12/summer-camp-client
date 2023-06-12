e5f90f69318445154781d224d786b8fedd57ff05
tarikul-islam <tariikul12@gmail.com>
2023-06-12 23:22:23 +0600

    finally done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Fade } from "react-awesome-reveal";
+import {  Zoom } from "react-awesome-reveal";
 import Banner from "../Banner/Banner";
 import ExtraFeature from "../ExtraFeature/ExtraFeature";
 import PopularClasses from "../PopularClasses/PopularClasses";
@@ -8,13 +8,13 @@ import PopularInstructor from "../PopularInstuctor/PopularInstructor";
 const Home = () => {
     return (
    
-        <Fade cascade damping={0.8}>
+        <Zoom cascade damping={0.1}>
        
            <Banner></Banner>
             <PopularClasses></PopularClasses>
             <PopularInstructor></PopularInstructor>
             <ExtraFeature></ExtraFeature>
-        </Fade>
+        </Zoom>
     );
 };
 
